9ef33353d059ad2a0001209dfc4015491dc9b19e
Marc Andri Fuchs <marconymous@outlook.com>
2021-08-26 23:01:29 +0200

    first

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.0"
+    application
+}
+
+group = "dev.marconymous"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.junit.jupiter:junit-jupiter:5.7.0")
+    testImplementation(kotlin("test-junit"))
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClassName = "MainKt"
+}
---
+++ src/main/kotlin/MakingChange.kt
@@ -0,0 +1,23 @@
+class MakingChange {
+        private val coins = arrayOf(500, 100, 25, 10, 5, 1)
+        fun change(amount: Int) {
+            var coinIndex = 0
+
+            fun subtract(amount: Int, coin: Int, coinCount: Int = 0): Int {
+                return when {
+                    amount == 0 -> {
+                        coinCount;
+                    }
+                    amount - coin < 0 -> {
+                        subtract(amount, coins[++coinIndex], coinCount)
+                    }
+                    else -> {
+                        subtract(amount-coin, coin, coinCount+1)
+                    }
+                }
+            }
+
+            val amtCoins = subtract(amount, coins[coinIndex])
+            println("Amount -> $amtCoins")
+        }
+}
